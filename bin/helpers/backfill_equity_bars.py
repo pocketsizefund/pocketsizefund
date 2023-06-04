f09d6271b48371412262ff1c84e410b1da4b2d23
forstmeier <john.forstmeier@gmail.com>
2023-06-03 22:51:58 -0400

    Refactor backfill helper script

--- bin/helpers/backfill_equity_bars.py
+++ bin/helpers/backfill_equity_bars.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pandas
 
 from pkg.config import config
@@ -14,7 +16,9 @@ storage_client = storage.Client(
 
 data_client = data.Client(
     alpha_vantage_api_key=samconfig_file.get_parameter('AlphaVantageAPIKey'),
-    log_progress=True,
+    alpaca_api_key=samconfig_file.get_parameter('AlpacaPaperAPIKey'),
+    alpaca_api_secret=samconfig_file.get_parameter('AlpacaPaperAPISecret'),
+    print_logs=True,
 )
 
 trade_client = trade.Client(
@@ -24,36 +28,88 @@ trade_client = trade.Client(
     is_paper=True,
 )
 
-tickers = trade_client.get_available_tickers()
+available_tickers = trade_client.get_available_tickers()
 
-equities_raw_bars = data_client.get_equities_raw_bars(
-    tickers=tickers,
+file_names = storage_client.list_file_names(
+    prefix=storage.PREFIX_EQUITY_BARS_PATH,
 )
 
-dataframes_by_ticker: list[pandas.DataFrame] = []
-for equity_raw_bars in equities_raw_bars:
-    dataframe = data_client.convert_equity_raw_bars_to_dataframe(
-        equity_raw_bars=equity_raw_bars,
+bars: pandas.DataFrame = None
+if len(file_names) == 0:
+    bars = data_client.get_all_equities_bars(
+        tickers=available_tickers,
     )
 
-    dataframes_by_ticker.append(dataframe)
+else:
+    bars_by_year = storage_client.load_dataframes(
+        prefix=storage.PREFIX_EQUITY_BARS_PATH,
+        file_names=file_names,
+    )
 
-dataframe = pandas.concat(dataframes_by_ticker)
+    old_bars = pandas.concat(bars_by_year.values())
 
-dataframe.drop_duplicates(subset=['timestamp', 'ticker'], inplace=True)
+    ticker_grouped_bars = old_bars.groupby('ticker')
+    most_recent_timestamps = ticker_grouped_bars['timestamp'].max()
 
-grouped_dataframe = dataframe.groupby(dataframe.timestamp.dt.year)
+    newest_timestamps = pandas.DataFrame({
+        'ticker': most_recent_timestamps.index,
+        'timestamp': most_recent_timestamps.values
+    })
 
-dataframes_by_year: dict[str, pandas.DataFrame] = {}
-for group_name in grouped_dataframe.groups.keys():
-    group = grouped_dataframe.get_group(group_name)
+    new_tickers = list(
+        set(available_tickers) - set(newest_timestamps['ticker']),
+    )
+    tickers_in_rows = newest_timestamps[newest_timestamps['ticker'].isin(
+        available_tickers,
+    )]
+    rows_not_in_tickers = newest_timestamps[~newest_timestamps['ticker'].isin(
+        available_tickers,
+    )]
+
+    start_at = min(
+        tickers_in_rows['timestamp'].min(),
+        rows_not_in_tickers['timestamp'].min(),
+    )
+
+    end_at = datetime.datetime.today()
+
+    update_tickers = tickers_in_rows['ticker'].tolist() \
+        + rows_not_in_tickers['ticker'].tolist()
+
+    new_bars = data_client.get_all_equities_bars(
+        tickers=new_tickers,
+    )
+
+    updated_bars = data_client.get_range_equities_bars(
+        tickers=update_tickers,
+        start_at=start_at,
+        end_at=end_at,
+    )
+
+    bars = pandas.concat(objs=[
+        old_bars,
+        new_bars,
+        updated_bars,
+    ]).drop_duplicates(
+        subset=[
+            'tickers',
+            'timestamp',
+        ],
+    )
+
+year_grouped_bars = bars.groupby(bars.timestamp.dt.year)
+
+bars_by_year: dict[str, pandas.DataFrame] = {}
+
+for group_name in year_grouped_bars.groups.keys():
+    group = year_grouped_bars.get_group(group_name)
     group = group.drop_duplicates()
 
-    group_year_file_name = '{}.csv.gz'.format(str(group_name))
+    group_year = str(group_name)
 
-    dataframes_by_year[group_year_file_name] = group
+    bars_by_year[group_year] = group
 
 storage_client.store_dataframes(
     prefix=storage.PREFIX_EQUITY_BARS_PATH,
-    dataframes=dataframes_by_year,
+    dataframes=bars_by_year,
 )
