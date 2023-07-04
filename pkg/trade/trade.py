0f02fe885346f36bafb2f67d528c28372d24f4f7
forstmeier <john.forstmeier@gmail.com>
2023-07-04 17:33:55 -0400

    Add get positions method to trade client

--- pkg/trade/trade.py
+++ pkg/trade/trade.py
@@ -1,4 +1,5 @@
-from alpaca.trading import client
+from alpaca.broker import client as broker_client
+from alpaca.trading import client as trading_client
 from alpaca.trading import enums
 from alpaca.trading import requests as alpaca_trading_requests
 import finnhub
@@ -10,15 +11,23 @@ class Client:
         finnhub_api_key: str,
         alpaca_api_key: str,
         alpaca_api_secret: str,
+        alpaca_account_id: str,
         is_paper: bool = True,
     ) -> None:
         self.finnhub_client = finnhub.Client(api_key=finnhub_api_key)
 
-        self.alpaca_trading_client = client.TradingClient(
+        self.alpaca_trading_client = trading_client.TradingClient(
             api_key=alpaca_api_key,
             secret_key=alpaca_api_secret,
             paper=is_paper,
         )
+
+        self.alpaca_broker_client = broker_client.BrokerClient(
+            api_key=alpaca_api_key,
+            secret_key=alpaca_api_secret,
+        )
+
+        self.alpaca_account_id = alpaca_account_id
 
     def get_available_tickers(self) -> list[str]:
         # GSPC is the S&P 500
@@ -43,3 +52,18 @@ class Client:
                 tickers.append(asset.symbol)
 
         return tickers
+
+    def get_positions(self) -> list[dict[str, any]]:
+        alpaca_positions = self.alpaca_trading_client.list_positions(
+            account_id=self.alpaca_account_id,
+        )
+
+        positions: dict[str, dict[str, any]] = {}
+        for alpaca_position in alpaca_positions:
+            positions[alpaca_position.symbol] = {
+                'quantity': alpaca_position.qty,
+                'side': alpaca_position.side,
+                'value': alpaca_position.market_value,
+            }
+
+        return positions
