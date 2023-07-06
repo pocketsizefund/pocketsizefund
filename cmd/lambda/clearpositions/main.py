dc3f6fe151aeb59d552a9a27ce9f1a745fec63bf
forstmeier <john.forstmeier@gmail.com>
2023-07-06 13:28:07 -0400

    Build initial clear positions Lambda handler

---
+++ cmd/lambda/clearpositions/main.py
@@ -0,0 +1,15 @@
+import os
+
+from pkg.trade import trade
+
+
+def handler(event: any, context: any) -> dict[str, any]:
+    trade_client = trade.Client(
+        finnhub_api_key=os.getenv('FINNHUB_API_KEY'),
+        alpaca_api_key_id=os.getenv('ALPACA_API_KEY_ID'),
+        alpaca_api_secret_key=os.getenv('ALPACA_API_SECRET_KEY'),
+        alpaca_account_id=os.getenv('ALPACA_ACCOUNT_ID'),
+        is_paper=os.getenv('IS_PAPER'),
+    )
+
+    trade_client.clear_positions()
