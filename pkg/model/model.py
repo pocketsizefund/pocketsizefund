fdd0ab4a32f8701987ba2b181ef1117d20d9d707
forstmeier <john.forstmeier@gmail.com>
2023-07-06 11:59:24 -0400

    Update model implementation for multi-ticker training/predicting

--- pkg/model/model.py
+++ pkg/model/model.py
@@ -18,33 +18,31 @@ class Client:
     def train_model(
         self,
         data: pandas.DataFrame,
-    ) -> keras.models.Sequential:
-        target_variable = 'close_price'
-        target_data = data[target_variable].values.reshape(-1, 1)
+    ) -> None:
+        target_data = data.groupby(
+            'ticker',
+        )['close_price'].apply(numpy.array).values
 
         scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
-        target_data_scaled = scaler.fit_transform(target_data)
+        target_data_scaled = scaler.fit_transform(
+            numpy.concatenate(target_data).reshape(-1, 1),
+        )
 
         train_inputs = []
         train_outputs = []
         for i in range(DAYS_TO_TRAIN, len(target_data_scaled)):
-            train_inputs.append(target_data_scaled[i - DAYS_TO_TRAIN:i, 0])
-            train_outputs.append(target_data_scaled[i, 0])
+            train_inputs.append(target_data_scaled[i - DAYS_TO_TRAIN:i])
+            train_outputs.append(target_data_scaled[i])
 
         train_input = numpy.array(train_inputs)
         train_output = numpy.array(train_outputs)
-
-        train_input_reshaped = numpy.reshape(
-            train_input,
-            (train_input.shape[0], train_input.shape[1], 1),
-        )
 
         model = keras.models.Sequential()
         model.add(
             keras.layers.LSTM(
                 units=50,
                 return_sequences=True,
-                input_shape=(train_input_reshaped.shape[1], 1),
+                input_shape=(train_input.shape[1], train_input.shape[2]),
             ),
         )
         model.add(keras.layers.LSTM(units=50))
@@ -52,11 +50,9 @@ class Client:
 
         model.compile(optimizer='adam', loss='mean_squared_error')
 
-        model.fit(train_input_reshaped, train_output, epochs=10, batch_size=32)
+        model.fit(train_input, train_output, epochs=10, batch_size=32)
 
         self.model = model
-
-        return model
 
     def save_model(
         self,
@@ -70,49 +66,48 @@ class Client:
     def load_model(
         self,
         path: str,
-    ) -> keras.models.Sequential:
+    ) -> None:
         model = keras.models.load_model(path)
 
         self.model = model
 
-        return model
-
     def get_model_predictions(
         self,
         data: pandas.DataFrame,
-    ):  # TEMP
+    ) -> dict[str, dict[str, any]]:
         if self.model is None:
             return NO_MODEL_EXCEPTION
 
         if len(data) < DAYS_TO_TRAIN:
             raise NOT_ENOUGH_DATA_EXCEPTION
 
-        target_variable = 'close_price'
-        target_data = data[target_variable].values.reshape(-1, 1)
-
         scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
-        target_data_scaled = scaler.fit_transform(target_data)
-        target_data_scaled_reshaped = numpy.reshape(
-            target_data_scaled,
-            (
-                1,
-                target_data_scaled.shape[0],
-                1,
-            ),
-        )
 
-        predictions = []
-        for _ in range(DAYS_TO_PREDICT):
-            prediction = self.model.predict(target_data_scaled_reshaped)
-            predictions.append(prediction)
-            target_data_scaled_reshaped = numpy.append(
-                target_data_scaled_reshaped[:, 1:, :],
-                prediction.reshape(1, 1, 1),
-                axis=1,
+        target_data = data['close_price'].values
+        target_data_scaled = scaler.fit_transform(target_data.reshape(-1, 1))
+
+        predictions: dict[str, dict[str, any]] = {}
+
+        for ticker in data['ticker'].unique():
+            ticker_data = data[data['ticker'] == ticker]
+            ticker_target_data_scaled = target_data_scaled[ticker_data.index]
+
+            inputs = ticker_target_data_scaled[-DAYS_TO_TRAIN:]
+
+            predicted_outputs = []
+            for _ in range(DAYS_TO_PREDICT):
+                input_reshaped = numpy.reshape(inputs, (1, DAYS_TO_TRAIN, 1))
+                prediction = self.model.predict(input_reshaped)[0][0]
+                predicted_outputs.append(prediction)
+
+                inputs = numpy.append(inputs[1:], prediction)
+
+            predicted_outputs_scaled = scaler.inverse_transform(
+                numpy.array(predicted_outputs).reshape(-1, 1),
             )
 
-        predictions = scaler.inverse_transform(
-            numpy.array(predictions).reshape(-1, 1),
-        )
+            predictions[ticker] = {
+                'predicted_closing_prices': predicted_outputs_scaled.flatten().tolist(),
+            }
 
-        return predictions.tolist()
+        return predictions
