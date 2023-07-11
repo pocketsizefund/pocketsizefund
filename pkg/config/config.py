6aecce2e8c480eead9a1da89c4cd45649db2559a
forstmeier <john.forstmeier@gmail.com>
2023-07-10 20:00:48 -0400

    Update config parsing with correct action key name

--- pkg/config/config.py
+++ pkg/config/config.py
@@ -14,7 +14,7 @@ class SAMConfig:
         self.samconfig_file = toml.load(file_path)
 
         self.parameters: dict[str, str] = {}
-        parameters = self.samconfig_file[environment]['build']['parameters']
+        parameters = self.samconfig_file[environment]['deploy']['parameters']
         for parameter in parameters['parameter_overrides']:
             parameter_split = parameter.split('=')
             self.parameters[parameter_split[0]] = parameter_split[1]
