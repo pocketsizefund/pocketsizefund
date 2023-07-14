8f4b625c47b26ae39c3e0528dfa118a569179834
forstmeier <john.forstmeier@gmail.com>
2023-07-14 13:17:18 -0400

    Rebuild initial create positions Lambda package

--- cmd/lambda/createpositions/main.py
+++ cmd/lambda/createpositions/main.py
@@ -1,51 +1,22 @@
 import os
-import datetime
+import heapq
+
+import pandas
 
 from pkg.storage import storage
-from pkg.model import model
 from pkg.trade import trade
+from pkg.model import model
+from pkg.portfolio import portfolio
+
+
+POSITIONS_COUNT = 10
+BUYING_POWER_PERCENTAGE = 0.95  # preventing overspend
 
 
 def handler(event: any, context: any) -> dict[str, any]:
     storage_client = storage.Client(
         s3_data_bucket_name=os.environ['S3_DATA_BUCKET_NAME'],
     )
-
-    file_name = str(datetime.datetime.now().year)
-
-    file_names = storage_client.list_file_names(
-        prefix=storage.PREFIX_EQUITY_BARS_PATH,
-    )
-
-    file_names.sort(reverse=True)
-
-    if file_name not in file_names:
-        file_name = file_names[0]
-
-    old_dataframes = storage_client.load_dataframes(
-        prefix=storage.PREFIX_EQUITY_BARS_PATH,
-        file_names=[file_name],
-    )
-
-    old_dataframe = old_dataframes[file_name]
-
-    old_dataframe_grouped_by_ticker = old_dataframe.groupby('ticker')
-
-    if old_dataframe_grouped_by_ticker.size() < model.DAYS_TO_TRAIN:
-        file_name = file_names[1]
-
-        old_dataframes = storage_client.load_dataframes(
-            prefix=storage.PREFIX_EQUITY_BARS_PATH,
-            file_names=[file_name],
-        )
-
-        old_dataframe = old_dataframes[file_name]
-
-    prediction_data = old_dataframe.head(model.DAYS_TO_TRAIN)
-
-    model_client = model.Client()
-
-    model_client.load_model(os.getenv('MODEL_FILE_NAME'))
 
     trade_client = trade.Client(
         finnhub_api_key=os.getenv('FINNHUB_API_KEY'),
@@ -55,29 +26,74 @@ def handler(event: any, context: any) -> dict[str, any]:
         is_paper=True if os.getenv('IS_PAPER') == 'true' else False,
     )
 
-    available_tickers = trade_client.get_available_tickers()
-
-    predictions_by_ticker = model_client.get_model_predictions(prediction_data)
-
-    predictions_by_ticker = predictions_by_ticker[predictions_by_ticker['ticker'].isin(
-        available_tickers),
-    ]
-
-    current_prices_by_ticker = trade_client.get_current_prices(
-        tickers=predictions_by_ticker['ticker'].tolist(),
+    model_client = model.Client(
+        file_path=os.getenv('MODEL_FILE_PATH'),
     )
 
-    buying_power = trade_client.get_buying_power()
+    portfolio_client = portfolio.Client()
 
-    positions: list[dict[str, any]] = []
-    for ticker, price_predictions in predictions_by_ticker.iterrows():
-        current_price = current_prices_by_ticker[ticker]
-        next_price = price_predictions[0]
+    file_names = storage_client.list_file_names(
+        prefix=storage.PREFIX_EQUITY_BARS_PATH,
+    )
 
-        positions.append({
+    old_equity_bars_by_year = storage_client.load_dataframes(
+        prefix=storage.PREFIX_EQUITY_BARS_PATH,
+        file_names=file_names,
+    )
+
+    old_bars = pandas.concat(old_equity_bars_by_year.values())
+
+    old_bars_grouped_by_ticker = old_bars.groupby('ticker')
+
+    available_tickers = trade_client.get_available_tickers()
+
+    prediction_data = old_bars_grouped_by_ticker.head(
+        model.DAYS_TO_TRAIN,
+    )
+
+    prediction_data_filtered = prediction_data[
+        prediction_data['ticker'].isin(available_tickers),
+    ]
+
+    predictions_by_ticker = model_client.get_model_predictions(
+        data=prediction_data_filtered,
+    )
+
+    current_prices_by_ticker = trade_client.get_current_prices(
+        tickers=list(predictions_by_ticker.keys()),
+    )
+
+    moves_by_ticker = {
+        ticker: predictions_by_ticker[ticker][0] -
+        current_prices_by_ticker[ticker]
+        for ticker in predictions_by_ticker
+    }
+
+    highest_moves_by_ticker = heapq.nlargest(
+        n=POSITIONS_COUNT,
+        iterable=moves_by_ticker.items(),
+        key=lambda item: item[1],
+    )
+
+    highest_moves_tickers = highest_moves_by_ticker.keys()
+
+    portfolio_data = old_bars_grouped_by_ticker.filter(
+        lambda x: x['ticker'].iloc[0] in highest_moves_tickers,
+    )
+
+    weights_by_ticker = portfolio_client.calculate_weights(
+        data=portfolio_data,
+    )
+
+    buying_power = trade_client.get_buying_power() * BUYING_POWER_PERCENTAGE
+
+    positions: list[dict[str, any]] = [
+        {
             'ticker': ticker,
-            'quantity': (buying_power / len(predictions_by_ticker)) / current_price,
-            'side': trade.SIDE_BUY if next_price > current_price else trade.SIDE_SELL,
-        })
+            'quantity': (buying_power * weight) / current_prices_by_ticker[ticker],
+            'side': trade.SIDE_BUY,
+        }
+        for ticker, weight in weights_by_ticker.items()
+    ]
 
     trade_client.set_positions(positions)
