4d5037d01a914769094981470d5b80b42013328a
forstmeier <john.forstmeier@gmail.com>
2023-08-01 20:34:38 -0400

    Add return type hint to message client method

--- pkg/message/message.py
+++ pkg/message/message.py
@@ -5,7 +5,7 @@ class Client:
     def __init__(
         self,
         topic_arn: str,
-    ):
+    ) -> None:
         self.sns_client = boto3.client('sns')
         self.topic_arn = topic_arn
 
@@ -13,7 +13,7 @@ class Client:
         self,
         subject: str,
         message: str,
-    ):
+    ) -> None:
         self.sns_client.publish(
             TopicArn=self.topic_arn,
             Subject=subject,
