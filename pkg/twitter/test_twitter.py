5099b1f34c67da4ec71f8e256af7fe777bef0dc9
forstmeier <john.forstmeier@gmail.com>
2023-08-05 18:29:35 -0400

    Build initial Twitter profile updater method

--- pkg/twitter/test_twitter.py
+++ pkg/twitter/test_twitter.py
@@ -4,15 +4,38 @@ from pkg.twitter import twitter
 
 
 class MockTwitterClient:
-    def __init__(
-        self,
-    ) -> None:
+    def __init__(self) -> None:
         pass
 
     def create_tweet(
         self,
         text: str,
     ) -> any:
+        return None
+
+
+class MockTwitterAPI:
+    def __init__(self) -> None:
+        pass
+
+    def update_profile(
+        self,
+        location: str,
+        description: str,
+        profile_link_color: str,
+    ) -> None:
+        return None
+
+    def update_profile_banner(
+        self,
+        filename: str,
+    ) -> None:
+        return None
+
+    def update_profile_image(
+        self,
+        filename: str,
+    ) -> None:
         return None
 
 
@@ -23,6 +46,7 @@ class TestSendTweet(unittest.TestCase):
             api_key_secret='api_key_secret',
             access_token='access_token',
             access_token_secret='access_token_secret',
+            image_files_path='image/files/path'
         )
 
         client.twitter_client = MockTwitterClient()
@@ -30,3 +54,18 @@ class TestSendTweet(unittest.TestCase):
         client.send_tweet(
             text='text',
         )
+
+
+class TestUpdateProfile(unittest.TestCase):
+    def test_update_profile_success(self):
+        client = twitter.Client(
+            api_key='api_key',
+            api_key_secret='api_key_secret',
+            access_token='access_token',
+            access_token_secret='access_token_secret',
+            image_files_path='images'
+        )
+
+        client.twitter_api = MockTwitterAPI()
+
+        client.update_profile(twitter.configuration_orange)
