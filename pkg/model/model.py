d79a35bc38306629eee3870cb75f8c8d08ee5f1b
forstmeier <john.forstmeier@gmail.com>
2023-09-09 18:21:37 -0400

    Remove hard-coded column names/write initial train model method

--- pkg/model/model.py
+++ pkg/model/model.py
@@ -1,13 +1,26 @@
 import pandas
 from sklearn import preprocessing
 import numpy
+import tensorflow
 
 
 DAYS_TO_TRAIN = 30
 
 
 class Client:
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        sort_column: str,  # timestamp
+        drop_columns: list[str],  # source
+        scale_columns: list[str],
+        group_column: str,  # ticker
+        target_column: str,  # close_price
+    ) -> None:
+        self.sort_column = sort_column
+        self.drop_columns = drop_columns
+        self.scale_columns = scale_columns
+        self.group_column = group_column
+        self.target_column = target_column
         self.model = None
 
     def preprocess_data(
@@ -15,12 +28,19 @@ class Client:
         data: pandas.DataFrame,
         training_percentage: float,
     ) -> dict[str: numpy.ndarray]:
+        data = data.drop(
+            columns=self.drop_columns,
+            axis=1,
+        )
+
         data.sort_values(
-            by='timestamp',
+            by=self.sort_column,
             inplace=True,
         )
 
-        data_grouped_by_ticker: pandas.DataFrame = data.groupby(by='ticker')
+        data_grouped_by_ticker: pandas.DataFrame = data.groupby(
+            by=self.group_column,
+        )
 
         def create_sequence(data, sequence_length):
             input_data = []
@@ -28,20 +48,12 @@ class Client:
             for i in range(len(data) - sequence_length):
                 input_data.append(data[i:i+sequence_length])
                 output_data.append(
-                    data.iloc[i+sequence_length]['close_price']
+                    data.iloc[i+sequence_length][self.target_column]
                 )
 
             return numpy.array(input_data), numpy.array(output_data)
 
         preprocessed_data: dict[str: dict[str: numpy.ndarray]] = {}
-
-        scaled_columns = [
-            'open_price',
-            'high_price',
-            'low_price',
-            'close_price',
-            'volume',
-        ]
 
         for ticker, ticker_data in data_grouped_by_ticker:
             split = int(len(ticker_data) * training_percentage)
@@ -53,12 +65,12 @@ class Client:
             scaled_training_data: pandas.DataFrame = training_data.copy()
             scaled_testing_data: pandas.DataFrame = testing_data.copy()
 
-            scaled_training_data[scaled_columns] = scaler.fit_transform(
-                training_data[scaled_columns]
+            scaled_training_data[self.scale_columns] = scaler.fit_transform(
+                training_data[self.scale_columns]
             )
 
-            scaled_testing_data[scaled_columns] = scaler.transform(
-                testing_data[scaled_columns]
+            scaled_testing_data[self.scale_columns] = scaler.transform(
+                testing_data[self.scale_columns]
             )
 
             input_training_data, output_training_data = create_sequence(
@@ -79,3 +91,42 @@ class Client:
             }
 
         return preprocessed_data
+
+    def train_model(
+        self,
+        data: pandas.DataFrame,
+    ) -> None:
+        model = tensorflow.keras.models.Sequential()
+
+        feature_count = len(self.scale_columns)
+        + self.sort_column
+        + self.group_column
+        + self.target_column
+
+        model.add(
+            tensorflow.keras.layers.LSTM(
+                units=100,
+                return_sequences=True,
+                input_shape=(DAYS_TO_TRAIN, feature_count),
+            )
+        )
+        model.add(
+            tensorflow.keras.layers.LSTM(
+                units=50,
+                return_sequences=True,
+            ),
+        )
+        model.add(
+            tensorflow.keras.layers.LSTM(
+                units=50,
+                return_sequences=True,
+            ),
+        )
+        model.add(tensorflow.keras.layers.Dense(units=1))
+
+        model.compile(
+            loss='mean_squared_error',
+            optimizer='adam',
+        )
+
+        self.model = model
