1b0adcf1cf7bff6960ae64788c216f49f139aed8
forstmeier <john.forstmeier@gmail.com>
2023-11-17 23:57:35 -0500

    Remove incorrectly added interal packages from model package

--- pkg/model/model.py
+++ pkg/model/model.py
@@ -1,52 +1,24 @@
-import datetime
-
 from sagemaker import tensorflow
-
-from pkg.trade import trade
-from pkg.data import data
+import pandas
 
 
 class Client:
     def __init__(
         self,
         model_endpoint_name: str,
-        darqube_api_key: str,
-        alpaca_api_key: str,
-        alpaca_api_secret: str,
-        alpha_vantage_api_key: str,
     ) -> None:
         self.predictor = tensorflow.TensorFlowPredictor(
             endpoint_name=model_endpoint_name,
         )
 
-        self.trade_client = trade.Client(
-            darqube_api_key=darqube_api_key,
-            alpaca_api_key=alpaca_api_key,
-            alpaca_api_secret=alpaca_api_secret,
-        )
-
-        self.data_client = data.Client(
-            alpha_vantage_api_key=alpha_vantage_api_key,
-            alpaca_api_key=alpaca_api_key,
-            alpaca_api_secret=alpaca_api_secret,
-        )
-
-    def generate_predictions(self) -> any:
-        available_tickers = self.trade_client.get_available_tickers()
-
-        end_at = datetime.datetime.now()
-        start_at = end_at - datetime.timedelta(days=1)
-
-        equity_bars = self.data_client.get_range_equities_bars(
-            tickers=available_tickers,
-            start_at=start_at,
-            end_at=end_at,
-        )
-
-        equity_bars['timestamp'] = equity_bars['timestamp'].astype(str)
+    def generate_predictions(
+        self,
+        data: pandas.DataFrame,
+    ) -> any:
+        data['timestamp'] = data['timestamp'].astype(str)
 
         predictions = self.predictor.predict(
-            data=equity_bars.to_dict(orient='records'),
+            data=data.to_dict(orient='records'),
         )
 
         return predictions
--- pkg/model/test_model.py
+++ pkg/model/test_model.py
@@ -1,5 +1,4 @@
 import unittest
-import datetime
 
 import pandas
 
@@ -23,46 +22,19 @@ class MockPredictor:
         }
 
 
-class MockTradeClient:
-    def __init__(self) -> None:
-        pass
-
-    def get_available_tickers(self) -> list[str]:
-        return ['AAPL']
-
-
-class MockDataClient:
-    def __init__(self) -> None:
-        pass
-
-    def get_range_equities_bars(
-        self,
-        tickers: list[str],
-        start_at: datetime,
-        end_at: datetime
-    ) -> pandas.DataFrame:
-        return pandas.DataFrame(
-            data={
-                'timestamp': pandas.Timestamp('2021-01-01'),
-            },
-            index=[0],
-        )
-
-
 class TestGeneratePredictions(unittest.TestCase):
     def test_generate_predictions_success(self):
         client = model.Client(
             model_endpoint_name='model_endpoint_name',
-            darqube_api_key='darqube_api_key',
-            alpaca_api_key='alpaca_api_key',
-            alpaca_api_secret='alpaca_api_secret',
-            alpha_vantage_api_key='alpha_vantage_api_key',
         )
 
         client.predictor = MockPredictor()
-        client.trade_client = MockTradeClient()
-        client.data_client = MockDataClient()
 
-        predictions = client.generate_predictions()
+        predictions = client.generate_predictions(data=pandas.DataFrame(
+            data={
+                'timestamp': pandas.Timestamp('2021-01-01'),
+            },
+            index=[0],
+        ))
 
         self.assertEqual(10.0, predictions['65'][0][0])
