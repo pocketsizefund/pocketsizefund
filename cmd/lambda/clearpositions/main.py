5a6f25aeef803a59987cee5692b7b0ed296a3e43
forstmeier <john.forstmeier@gmail.com>
2024-02-22 22:02:49 -0500

    Remove unused message package

--- cmd/lambda/clearpositions/main.py
+++ cmd/lambda/clearpositions/main.py
@@ -1,7 +1,6 @@
 import os
 
 from pkg.trade import trade
-from pkg.message import message
 
 
 trade_client = trade.Client(
@@ -11,23 +10,10 @@ trade_client = trade.Client(
     is_paper=True if os.getenv('IS_PAPER') == 'true' else False,
 )
 
-message_client = message.Client(
-    topic_arn=os.getenv('SNS_ERRORS_TOPIC_ARN'),
-)
-
 
 def handler(event: any, context: any) -> dict[str, any]:
-    try:
-        market_status = trade_client.get_market_status()
-        if not market_status['is_market_open']:
-            raise Exception('market is closed')
+    market_status = trade_client.get_market_status()
+    if not market_status['is_market_open']:
+        raise Exception('market is closed')
 
-        trade_client.clear_positions()
-
-    except Exception as error:
-        message_client.send_message(
-            subject='Clear positions Lambda errored',
-            message='Error: {}'.format(error),
-        )
-
-        raise error
+    trade_client.clear_positions()
--- cmd/lambda/createpositions/main.py
+++ cmd/lambda/createpositions/main.py
@@ -5,7 +5,6 @@ from pkg.storage import storage
 from pkg.trade import trade
 from pkg.data import data
 from pkg.model import model
-from pkg.message import message
 
 
 POSITIONS_COUNT = 10
@@ -31,87 +30,74 @@ model_client = model.Client(
     model_endpoint_name=os.getenv('MODEL_ENDPOINT_NAME'),
 )
 
-message_client = message.Client(
-    topic_arn=os.getenv('SNS_ERRORS_TOPIC_ARN'),
-)
-
 
 def handler(event: any, context: any) -> dict[str, any]:
-    try:
-        market_status = trade_client.get_market_status()
-        if not market_status['is_market_open']:
-            raise Exception('market is closed')
+    market_status = trade_client.get_market_status()
+    if not market_status['is_market_open']:
+        raise Exception('market is closed')
 
-        available_tickers = trade_client.get_available_tickers()
+    available_tickers = trade_client.get_available_tickers()
 
-        end_at = datetime.datetime.now()
-        start_at = end_at - datetime.timedelta(days=50)
+    end_at = datetime.datetime.now()
+    start_at = end_at - datetime.timedelta(days=50)
 
-        prediction_data = data_client.get_range_equities_bars(
-            tickers=available_tickers,
-            start_at=start_at,
-            end_at=end_at,
-        )
+    prediction_data = data_client.get_range_equities_bars(
+        tickers=available_tickers,
+        start_at=start_at,
+        end_at=end_at,
+    )
 
-        prediction_data = prediction_data.sort_values(
-            by='timestamp',
-            ascending=False,
-        ).groupby('ticker').head(30).reset_index(drop=True)
+    prediction_data = prediction_data.sort_values(
+        by='timestamp',
+        ascending=False,
+    ).groupby('ticker').head(30).reset_index(drop=True)
 
-        prediction_data['timestamp'] = prediction_data['timestamp'].astype(str)
+    prediction_data['timestamp'] = prediction_data['timestamp'].astype(str)
 
-        predictions_by_ticker = model_client.generate_predictions(
-            data=prediction_data,
-        )
+    predictions_by_ticker = model_client.generate_predictions(
+        data=prediction_data,
+    )
 
-        current_prices_by_ticker = trade_client.get_current_prices(
-            tickers=list(predictions_by_ticker.keys()),
-        )
+    current_prices_by_ticker = trade_client.get_current_prices(
+        tickers=list(predictions_by_ticker.keys()),
+    )
 
-        moves_by_ticker = {
-            ticker: predictions_by_ticker[ticker][0][0] -
-            current_prices_by_ticker[ticker]['price']
-            for ticker in predictions_by_ticker
+    moves_by_ticker = {
+        ticker: predictions_by_ticker[ticker][0][0] -
+        current_prices_by_ticker[ticker]['price']
+        for ticker in predictions_by_ticker
+    }
+
+    sorted_moves_by_ticker = dict(sorted(
+        moves_by_ticker.items(),
+        key=lambda item: item[1], reverse=True,
+    ))
+
+    highest_moves_by_ticker = {
+        k: sorted_moves_by_ticker[k]
+        for k in list(sorted_moves_by_ticker)[:POSITIONS_COUNT]
+    }
+
+    highest_moves_tickers = highest_moves_by_ticker.keys()
+
+    if len(highest_moves_tickers) == 0:
+        raise Exception('no tickers to trade')
+
+    weights_by_ticker = {
+        ticker: 1 / len(highest_moves_tickers)
+        for ticker in highest_moves_tickers
+    }
+
+    available_cash = trade_client.get_available_cash()
+
+    positions: list[dict[str, any]] = [
+        {
+            'ticker': ticker_weight[0],
+            'quantity': (available_cash * ticker_weight[1]) /
+            current_prices_by_ticker[ticker_weight[0]]['price'],
+            'side': trade.SIDE_BUY,
         }
+        for ticker_weight in weights_by_ticker.items()
+    ]
 
-        sorted_moves_by_ticker = dict(sorted(
-            moves_by_ticker.items(),
-            key=lambda item: item[1], reverse=True,
-        ))
-
-        highest_moves_by_ticker = {
-            k: sorted_moves_by_ticker[k]
-            for k in list(sorted_moves_by_ticker)[:POSITIONS_COUNT]
-        }
-
-        highest_moves_tickers = highest_moves_by_ticker.keys()
-
-        if len(highest_moves_tickers) == 0:
-            raise Exception('no tickers to trade')
-
-        weights_by_ticker = {
-            ticker: 1 / len(highest_moves_tickers)
-            for ticker in highest_moves_tickers
-        }
-
-        available_cash = trade_client.get_available_cash()
-
-        positions: list[dict[str, any]] = [
-            {
-                'ticker': ticker_weight[0],
-                'quantity': (available_cash * ticker_weight[1]) /
-                current_prices_by_ticker[ticker_weight[0]]['price'],
-                'side': trade.SIDE_BUY,
-            }
-            for ticker_weight in weights_by_ticker.items()
-        ]
-
-        trade_client.set_positions(positions)
-
-    except Exception as error:
-        message_client.send_message(
-            subject='Create positions Lambda errored',
-            message='Error: {}'.format(error),
-        )
-
-        raise error
+    trade_client.set_positions(positions)
