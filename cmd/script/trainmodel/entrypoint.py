900e4ba2eb8da6df5b6d781e51b5fbfa60a24f4c
chrisaddy <chris.william.addy@gmail.com>
2024-04-25 18:12:30 -0400

    cmds have been moved to prefect pipelines

--- cmd/script/trainmodel/entrypoint.py
+++ cmd/script/trainmodel/entrypoint.py
@@ -7,53 +7,53 @@ from pkg.storage import storage
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
-    '--s3-data-bucket-name',
+    "--s3-data-bucket-name",
     type=str,
-    dest='s3_data_bucket_name',
+    dest="s3_data_bucket_name",
 )
 
 parser.add_argument(
-    '--s3-artifacts-bucket-name',
+    "--s3-artifacts-bucket-name",
     type=str,
-    dest='s3_artifacts_bucket_name',
+    dest="s3_artifacts_bucket_name",
 )
 
 parser.add_argument(
-    '--model_dir',  # underscore required for SageMaker
+    "--model_dir",  # underscore required for SageMaker
     type=str,
-    dest='model_dir',
+    dest="model_dir",
 )
 
 parser.add_argument(
-    '--epochs',
+    "--epochs",
     type=int,
-    dest='epochs',
+    dest="epochs",
 )
 
 parser.add_argument(
-    '--days',
+    "--days",
     type=int,
-    dest='days',
+    dest="days",
 )
 
 parser.add_argument(
-    '--available-tickers',
+    "--available-tickers",
     type=str,
-    dest='available_tickers',
+    dest="available_tickers",
 )
 
 parser.add_argument(
-    '--notes',
+    "--notes",
     type=str,
-    dest='notes',
+    dest="notes",
 )
 
 arguments = parser.parse_args()
 
-secrets_file = open('secrets.env', 'r')
+secrets_file = open("secrets.env", "r")
 secrets_content = secrets_file.read()
 secrets_file.close()
-weights_and_biases_api_key = secrets_content.split('=')[1].strip()
+weights_and_biases_api_key = secrets_content.split("=")[1].strip()
 
 storage_client = storage.Client(
     s3_data_bucket_name=arguments.s3_data_bucket_name,
@@ -81,38 +81,32 @@ features_by_version = storage_client.load_dataframes(
 
 training_features = features_by_version[max_version_file_name]
 
-available_tickers = arguments.available_tickers.split(',')
+available_tickers = arguments.available_tickers.split(",")
 
-filtered_features = training_features[training_features['ticker'].isin(
-    available_tickers
-)]
+filtered_features = training_features[
+    training_features["ticker"].isin(available_tickers)
+]
 
-most_recent_filtered_features = filtered_features.groupby('ticker').apply(
-    lambda group: group.nlargest(arguments.days, 'timestamp')
-).reset_index(drop=True)
+most_recent_filtered_features = (
+    filtered_features.groupby("ticker")
+    .apply(lambda group: group.nlargest(arguments.days, "timestamp"))
+    .reset_index(drop=True)
+)
 
 preprocessed_features = model_model.preprocess_training_features(
     data=most_recent_filtered_features,
 )
 
 training_output = model_model.train_model(
-    features=preprocessed_features['data'],
+    features=preprocessed_features["data"],
     epochs=arguments.epochs,
 )
 
 model_model.save_model(
-    model=training_output['model'],
-)
-
-model_model.save_metrics(
-    metrics=training_output['metrics'],
-)
-
-model_model.save_scalers(
-    scalers=preprocessed_features['scalers'],
+    model=training_output["model"],
 )
 
 model_model.save_data(
-    name='testing_data',
-    data=preprocessed_features['data']['testing'],
+    name="testing_data",
+    data=preprocessed_features["data"]["testing"],
 )
