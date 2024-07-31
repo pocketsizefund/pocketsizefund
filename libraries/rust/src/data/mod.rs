c38fdf4855499de42459ccbe511d25bc110e4d18
chrisaddy <chris.william.addy@gmail.com>
2024-07-31 13:58:24 -0400

    work on rust library

--- libraries/rust/src/data/mod.rs
+++ libraries/rust/src/data/mod.rs
@@ -1,3 +1,12 @@
+///! pocketsize fund data
 pub mod client;
 
 pub use client::Client;
+
+pub mod asset_class;
+pub mod bar;
+pub mod exchange;
+pub mod locale;
+pub mod market;
+pub mod prelude;
+pub mod ticker;
