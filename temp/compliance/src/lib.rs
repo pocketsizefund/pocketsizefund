83fd711e5276b8d5bb2df46cd9550fe47063add1
Chris Addy <chrisaddy@macbook-air.mynetworksettings.com>
2024-08-31 13:03:20 -0400

    testing

---
+++ temp/compliance/src/lib.rs
@@ -0,0 +1,20 @@
+pub mod routes;
+use actix_web::dev::Server;
+use actix_web::middleware::Logger;
+use actix_web::{App, HttpServer};
+use std::net::TcpListener;
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+
+    let server = HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .service(routes::health::check)
+            .service(routes::events::handler)
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
