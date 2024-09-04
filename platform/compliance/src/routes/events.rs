fc44919282696f4123faf4cc7d53bf028c8f63bf
chrisaddy <chris.william.addy@gmail.com>
2024-09-03 21:31:22 -0400

    remove platform

--- platform/compliance/src/routes/events.rs
+++
@@ -1,8 +0,0 @@
-use actix_web::post;
-use cloudevents::Event;
-use pocketsizefund::events::Market;
-
-#[post("/")]
-pub async fn handler(event: Event) -> Event {
-    event
-}
--- platform/compliance/src/routes/health.rs
+++
@@ -1,6 +0,0 @@
-use actix_web::{get, HttpResponse};
-
-#[get("/health")]
-pub async fn check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
