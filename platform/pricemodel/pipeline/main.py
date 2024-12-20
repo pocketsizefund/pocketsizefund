dc65baab59a5c41753f25488466212b3385ea992
chrisaddy <chris.william.addy@gmail.com>
2024-12-20 15:09:07 -0500

    update data pipeline

--- platform/pricemodel/pipeline/main.py
+++ platform/pricemodel/pipeline/main.py
@@ -1,21 +1,23 @@
 from trainer import Trainer
 from model import SimpleModel
-from data import load_data, process_data, train_val_split
+from data import load_data, prepare_data, split_data
 from tinygrad import nn
 
 
 if __name__ == "__main__":
-    data = load_data()
-    print(data)
-    data = process_data(data)
+    data = load_data("../consolidated_data.csv")
+    data, static_features, encoder = prepare_data(data)
 
-    print(data)
+    train, val = split_data(data)
 
-    train, val = train_val_split(data)
+    print(train.numpy())
+    print(train.shape)
+    print(val.shape)
+
 
     trainer = Trainer(
         model = SimpleModel,
-        hyperparameters = {"input_size": 2, "hidden_size": 1},
+        hyperparameters = {"input_size": 6, "hidden_size": 6},
         optimizer = nn.optim.Adam,
         max_epochs = 5,
         batch_size = 32,
