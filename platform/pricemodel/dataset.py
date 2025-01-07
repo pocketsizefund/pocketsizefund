d320b2c61ab55434f4dd27a8fd96e1cbcec711d3
John Forstmeier <john.forstmeier@gmail.com>
2025-01-06 22:35:13 -0500

    Expand initial TFT rebuild work

--- platform/pricemodel/dataset.py
+++ platform/pricemodel/dataset.py
@@ -1,7 +1,17 @@
 import polars as pl
 import category_encoders as ce
 from tinygrad import Tensor
-from typing import Iterable, Dict, Union
+from typing import Iterable, Dict, List, Any
+
+
+continuous_variable_columns = [
+    "open_price",
+    "high_price",
+    "low_price",
+    "close_price",
+    "volume",
+    "volume_weighted_average_price",
+]
 
 
 class DataSet:
@@ -9,36 +19,41 @@ class DataSet:
         self,
         batch_size: int,
         sample_count: int,
-        means: int = None,
-        standard_deviations: int = None,
+        scalers: Dict[str, Dict[str, List[float]]] = None,  # TODO: change type
     ) -> None:
         self.batch_size = batch_size
         self.sample_count = sample_count
-        self.means = means
-        self.standard_deviations = standard_deviations
+        self.scalers = scalers
 
+    # TODO: change scaling to be grouped by ticker
+    # TODO: add padding for all skipped days (e.g. weekends + holidays)
+    # TODO: predict for 7 days (if padding is included)
+    # TODO: add "day of week" feature
+    # TODO: add "day of month" feature
+    # TODO: add "month" feature
+    # TODO: add "day of year" feature
+    # TODO: add "year" feature
     def set_scalers(self, data: pl.DataFrame) -> None:
-        self.skipped_columns = ["ticker", "timestamp"]
+        self.scalers = {}  # TODO: add type
+        for ticker, group in data.group_by("ticker"):
+            means = group[continuous_variable_columns].mean()
+            standard_deviations = group[continuous_variable_columns].std()
 
-        filtered_data = data.drop(self.skipped_columns)
-
-        self.means = filtered_data.select(
-            [pl.col(c).mean().alias(c) for c in filtered_data.columns]
-        )
-
-        self.standard_deviations = filtered_data.select(
-            [pl.col(c).std().alias(c) for c in filtered_data.columns]
-        )
+            self.scalers[ticker] = {"means": means, "standard_deviations": standard_deviations}
 
     def set_data(self, data: pl.DataFrame) -> None:
-        if self.means is None or self.standard_deviations is None:
-            raise ValueError("Scaler parameters have not been set")
+        if self.scalers is None:
+            raise ValueError("Scalers attribute has not been set")
 
-        filtered_data = data.drop(self.skipped_columns)
+        for ticker, group in data.group_by("ticker"):
+            means = self.scalers[ticker]["means"]
+            standard_deviations = self.scalers[ticker]["standard_deviations"]
 
-        textual_data = data.select(self.skipped_columns)
+            data.loc[group.index, continuous_variable_columns] = (
+                group[continuous_variable_columns] - means
+            ) / standard_deviations
 
-        ticker_series = textual_data["ticker"].to_pandas()
+        ticker_series = data["ticker"].to_pandas()
         encoder = ce.OrdinalEncoder(
             cols=["ticker"],
             handle_unknown="use_encoded_value",
@@ -46,35 +61,36 @@ class DataSet:
         )
         encoded_tickers = encoder.fit_transform(ticker_series)
 
-        textual_data = textual_data.with_columns(pl.Series("ticker", encoded_tickers["ticker"]))
+        data = data.with_columns(pl.Series("ticker", encoded_tickers["ticker"]))
 
-        textual_data = textual_data.with_columns(
+        data = data.with_columns(
             pl.col("timestamp").str.strptime(pl.Datetime, format="%Y-%m-%dT%H:%M:%SZ")
         )
 
-        textual_data = textual_data.with_columns(
-            pl.col("timestamp").dt.timestamp().alias("timestamp")
+        self.data = data
+
+    def get_information(self) -> Dict[str, Any]:  # TODO: change type
+        if self.scalers is None:
+            raise ValueError("Scalers attribute has not been set")
+
+        if self.data is None:
+            raise ValueError("Data attribute has not been set")
+
+        means_by_ticker = ({ticker: values["means"] for ticker, values in self.scalers.items()},)
+        standard_deviations_by_ticker = (
+            {ticker: values["standard_deviations"] for ticker, values in self.scalers.items()},
         )
 
-        temporal_data = filtered_data.select(
-            [
-                (pl.col(c) - self.means[c]) / self.standard_deviations[c]
-                for c in filtered_data.columns
-            ]
-        )
-
-        normalized_data = pl.concat([textual_data, temporal_data], how="horizontal")
-
-        self.data = normalized_data
-
-    def get_information(self) -> Dict[str, Union[str, int]]:
+        # TODO: convert to tensors (?)
         return {
-            "means": self.means,  # NOTE: make Tensor (?)
-            "standard_deviations": self.standard_deviations,  # NOTE: make Tensor (?)
-            # "encoder_lengths": [],  # TEMP
-            # "decoder_lengths": [],  # TEMP
-            # "encoder_categories": Tensor,
-            # "decoder_categories": Tensor,
+            "means_by_ticker": means_by_ticker,
+            "standard_deviations_by_ticker": standard_deviations_by_ticker,
+            "encoder_lengths": [30] * len(self.data),
+            "decoder_lengths": [5] * len(self.data),
+            "encoder_categories": self.data[["ticker"]].values[:30],
+            "decoder_categories": self.data[["ticker"]].values[-5:],
+            "encoder_continous_variables": self.data[continuous_variable_columns].values[:30],
+            "decoder_continous_variables": self.data[continuous_variable_columns].values[-7:],
         }
 
     def __iter__(self) -> Iterable[Tensor]:
