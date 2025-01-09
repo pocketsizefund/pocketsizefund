82a962c7d0edb9d4f6868433f9a2d91bf58d51dc
John Forstmeier <john.forstmeier@gmail.com>
2025-01-08 21:11:07 -0500

    Add partial fixes for DataSet class

--- platform/pricemodel/dataset.py
+++ platform/pricemodel/dataset.py
@@ -1,8 +1,10 @@
 import polars as pl
 import category_encoders as ce
 from tinygrad import Tensor
-from typing import Iterable, Dict, List, Any
+from typing import Iterable, Dict, List
+import warnings
 
+warnings.filterwarnings("ignore", category=FutureWarning, message=".*_get_tags.*")
 
 continuous_variable_columns = [
     "open_price",
@@ -19,57 +21,108 @@ class DataSet:
         self,
         batch_size: int,
         sample_count: int,
-        scalers: Dict[str, Dict[str, List[float]]] = None,  # TODO: change type
+        scalers: Dict[str, Dict[str, Tensor]] = None,
     ) -> None:
         self.batch_size = batch_size
         self.sample_count = sample_count
         self.scalers = scalers
-
-    # TODO: change scaling to be grouped by ticker
-    # TODO: add padding for all skipped days (e.g. weekends + holidays)
-    # TODO: predict for 7 days (if padding is included)
-    # TODO: add "day of week" feature
-    # TODO: add "day of month" feature
-    # TODO: add "month" feature
-    # TODO: add "day of year" feature
-    # TODO: add "year" feature
-    def set_scalers(self, data: pl.DataFrame) -> None:
-        self.scalers = {}  # TODO: add type
-        for ticker, group in data.group_by("ticker"):
-            means = group[continuous_variable_columns].mean()
-            standard_deviations = group[continuous_variable_columns].std()
-
-            self.scalers[ticker] = {"means": means, "standard_deviations": standard_deviations}
+        self.ticker_encoder = None
 
     def set_data(self, data: pl.DataFrame) -> None:
-        if self.scalers is None:
-            raise ValueError("Scalers attribute has not been set")
+        data = data.with_columns(
+            [
+                pl.col("timestamp").str.strptime(pl.Datetime).cast(pl.Date),  # , "%Y-%m-%d"),
+                pl.col("open_price").cast(pl.Float64).alias("open_price"),
+                pl.col("high_price").cast(pl.Float64).alias("high_price"),
+                pl.col("low_price").cast(pl.Float64).alias("low_price"),
+                pl.col("close_price").cast(pl.Float64).alias("close_price"),
+                pl.col("volume").cast(pl.Float64),
+                pl.col("volume_weighted_average_price").cast(pl.Float64),
+                pl.col("ticker").cast(pl.Utf8),
+            ]
+        )
 
-        for ticker, group in data.group_by("ticker"):
-            means = self.scalers[ticker]["means"]
-            standard_deviations = self.scalers[ticker]["standard_deviations"]
+        data = data.unique(subset=["ticker", "timestamp"])
 
-            data.loc[group.index, continuous_variable_columns] = (
-                group[continuous_variable_columns] - means
-            ) / standard_deviations
+        tickers = data.select("ticker").unique()
+        min_date = data.select(pl.col("timestamp").min())[0, 0]
+        max_date = data.select(pl.col("timestamp").max())[0, 0]
+
+        full_dates = pl.DataFrame(
+            {
+                "timestamp": pl.date_range(
+                    start=min_date,
+                    end=max_date,
+                    interval="1d",
+                    closed="both",
+                    eager=True,
+                )
+            }
+        )
+
+        full_df = tickers.join(full_dates, how="cross")
+
+        full_df = full_df.with_columns(
+            [pl.col("timestamp").rank(method="dense").cast(pl.Int32).alias("time_idx") - 1]
+        )
+
+        data = full_df.join(data, on=["ticker", "timestamp"], how="left")
+
+        data = data.sort(["ticker", "timestamp"])
+
+        data = data.with_columns(pl.col("timestamp").dt.weekday().alias("day_of_week"))
+
+        data = data.group_by("ticker").map_groups(
+            lambda df: df.sort("timestamp").with_columns(
+                [
+                    pl.col(col)
+                    .interpolate()
+                    .fill_null(strategy="forward")
+                    .fill_null(strategy="backward")
+                    for col in (continuous_variable_columns + ["day_of_week"])
+                ]
+            )
+        )
 
         ticker_series = data["ticker"].to_pandas()
-        encoder = ce.OrdinalEncoder(
+        ticker_encoder = ce.OrdinalEncoder(
             cols=["ticker"],
             handle_unknown="use_encoded_value",
             handle_missing="use_encoded_value",
         )
-        encoded_tickers = encoder.fit_transform(ticker_series)
+        self.ticker_encoder = ticker_encoder
+        encoded_tickers = ticker_encoder.fit_transform(ticker_series)
 
         data = data.with_columns(pl.Series("ticker", encoded_tickers["ticker"]))
 
-        data = data.with_columns(
-            pl.col("timestamp").str.strptime(pl.Datetime, format="%Y-%m-%dT%H:%M:%SZ")
-        )
+        if self.scalers is None:
+            self.scalers: Dict[str, Dict[str, Tensor]] = {}
+            for ticker, group in data.group_by("ticker"):
+                means = group[continuous_variable_columns].mean()
+                standard_deviations = group[continuous_variable_columns].std()
 
-        self.data = data
+                self.scalers[ticker] = {
+                    "means": Tensor(means.to_numpy()),
+                    "standard_deviations": Tensor(standard_deviations.to_numpy()),
+                }
 
-    def get_information(self) -> Dict[str, Any]:  # TODO: change type
+        groups: List[Tensor] = []
+        for ticker, group in data.group_by("ticker"):
+            means = self.scalers[ticker]["means"]
+            standard_deviations = self.scalers[ticker]["standard_deviations"]
+
+            group_data = Tensor(group.select(continuous_variable_columns).to_numpy())
+
+            scaled_group = (group_data.sub(means)).div(standard_deviations)
+
+            groups.append(scaled_group)
+
+        output_data = Tensor.empty(groups[0].shape)
+        output_data = output_data.cat(*groups, dim=0)
+
+        self.data = output_data.numpy()  # FIX
+
+    def get_information(self) -> Dict[str, Tensor]:
         if self.scalers is None:
             raise ValueError("Scalers attribute has not been set")
 
@@ -81,16 +134,19 @@ class DataSet:
             {ticker: values["standard_deviations"] for ticker, values in self.scalers.items()},
         )
 
-        # TODO: convert to tensors (?)
         return {
             "means_by_ticker": means_by_ticker,
             "standard_deviations_by_ticker": standard_deviations_by_ticker,
-            "encoder_lengths": [30] * len(self.data),
-            "decoder_lengths": [5] * len(self.data),
-            "encoder_categories": self.data[["ticker"]].values[:30],
-            "decoder_categories": self.data[["ticker"]].values[-5:],
-            "encoder_continous_variables": self.data[continuous_variable_columns].values[:30],
-            "decoder_continous_variables": self.data[continuous_variable_columns].values[-7:],
+            "encoder_lengths": Tensor([30] * len(self.data)),
+            "decoder_lengths": Tensor([5] * len(self.data)),
+            "encoder_categories": Tensor(self.data[["ticker"]].values[:30]),
+            "decoder_categories": Tensor(self.data[["ticker"]].values[-5:]),
+            "encoder_continous_variables": Tensor(
+                self.data[continuous_variable_columns].values[:30]
+            ),
+            "decoder_continous_variables": Tensor(
+                self.data[continuous_variable_columns].values[-7:]
+            ),
         }
 
     def __iter__(self) -> Iterable[Tensor]:
@@ -98,7 +154,20 @@ class DataSet:
             batch = self.data[i : i + self.batch_size]
             if batch.shape[0] < self.batch_size:
                 padding = Tensor.zeros((self.batch_size - batch.shape[0], *batch.shape[1:]))
-                batch = Tensor(data=batch.to_numpy())
+                # batch = Tensor(data=batch.to_numpy())
                 batch = batch.stack(padding, dim=0)
 
             yield Tensor(data=batch.to_numpy())
+
+
+# NOTE: testing
+
+dataset = DataSet(batch_size=32, sample_count=1000)
+
+dataframe = pl.read_csv("platform/pricemodel/consolidated_data.csv")
+
+dataset.set_data(dataframe)
+
+information = dataset.get_information()
+
+print(information)
