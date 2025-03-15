10ea76de50717a2757f1111398e27ce0efc7f48e
Chris Addy <chrisaddy@Chriss-MacBook-Air.local>
2025-03-15 14:00:57 -0400

    add in secrets and images

--- workflows/consolidate_data.py
+++ workflows/consolidate_data.py
@@ -1,19 +1,40 @@
-from union import workflow, task
+from union import workflow, task, Secret, ImageSpec, current_context
 import s3fs
 
+s3_image = ImageSpec(
+    name="pocketsizefund/s3",
+    platform="linux/amd64",
+    packages=[
+        "polars",
+        "pandas",
+        "pandera[polars]",
+        "flytekitplugins-pandera",
+        "s3fs",
+    ],
+)
 
-@task
-def list_available_data(
-    *, bucket: str, prefix: str = "", file_extension: str = "parquet"
-) -> list[str]:
-    fs = s3fs.S3FileSystem(anon=False)
+
+@task(
+    container_image=s3_image,
+    secret_requests=[
+        Secret(key="AWS_ACCESS_KEY_ID"),
+        Secret(key="AWS_SECRET_ACCESS_KEY"),
+        Secret(key="DATA_BUCKET"),
+    ],
+)
+def list_available_data(*, prefix: str = "", file_extension: str = "parquet") -> list[str]:
+    AWS_SECRET_ACCESS_KEY = current_context().secrets.get(key="AWS_SECRET_ACCESS_KEY")
+    AWS_ACCESS_KEY_ID = current_context().secrets.get(key="AWS_ACCESS_KEY_ID")
+    BUCKET = current_context().secrets.get(key="DATA_BUCKET")
+
+    fs = s3fs.S3FileSystem(secret=AWS_SECRET_ACCESS_KEY, key=AWS_ACCESS_KEY_ID)
     return [
         f"s3://{path}"
-        for path in fs.find(f"{bucket}/{prefix}", maxdepth=None)
-        if path.endswth(file_extension)
+        for path in fs.find(f"{BUCKET}/{prefix}", maxdepth=None)
+        if path.endswith(file_extension)
     ]
 
 
 @workflow
-def shape_bars_data(bucket: str) -> list[str]:
-    return list_available_data(bucket=bucket)
+def shape_bars_data() -> list[str]:
+    return list_available_data()
