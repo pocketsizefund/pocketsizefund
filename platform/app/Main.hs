7d7bcb759ad4454454d4ef543e1eafecdef714c2
Chris Addy <chrisaddy@Chriss-MacBook-Air.local>
2025-04-10 14:15:07 -0400

    add linter functions to mise and github action for both python and haskell

--- platform/app/Main.hs
+++ platform/app/Main.hs
@@ -1,13 +1,10 @@
 module Main (main) where
-import Network.Wai.Handler.Warp (run, runSettings, defaultSettings, setLogger, setPort)
-import Network.Wai.Logger (withStdoutLogger)
-
-
 
 import Lib
+import Network.Wai.Handler.Warp (defaultSettings, run, runSettings, setLogger, setPort)
+import Network.Wai.Logger (withStdoutLogger)
 
 main :: IO ()
 main = withStdoutLogger $ \aplogger -> do
   let settings = setPort 8080 $ setLogger aplogger defaultSettings
   runSettings settings app
-
--- platform/test/Spec.hs
+++ platform/test/Spec.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
 import Lib (app)
@@ -11,9 +11,6 @@ main = hspec spec
 
 spec :: Spec
 spec = with (return app) $ do
-    describe "GET /users" $ do
-        it "responds with 200" $ do
-            get "/users" `shouldRespondWith` 200
-        it "responds with [User]" $ do
-            let users = "[{\"userId\":1,\"userFirstName\":\"Isaac\",\"userLastName\":\"Newton\"},{\"userId\":2,\"userFirstName\":\"Albert\",\"userLastName\":\"Einstein\"}]"
-            get "/users" `shouldRespondWith` users
+  describe "GET /health" $ do
+    it "responds with 200" $ do
+      get "/health" `shouldRespondWith` 200
