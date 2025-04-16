fa0ff054a16c96eb80a39964f285677b4d23b61a
Chris Addy <chrisaddy@Chriss-MacBook-Air.local>
2025-04-15 21:22:53 -0400

    add test github action

--- platform/src/Account.hs
+++ platform/src/Account.hs
@@ -1,12 +1,11 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 
-module Account (Account(..)) where
+module Account (Account (..)) where
 
-import GHC.Generics (Generic)
 import Data.Aeson
 import Data.Fixed (Centi)
+import GHC.Generics (Generic)
 import Text.Read (readMaybe)
 
 type Money = Centi
@@ -14,21 +13,19 @@ type Money = Centi
 data Currency = USD
   deriving (Show, Read, Eq, Generic, FromJSON, ToJSON)
 
-data AdminConfigurations = AdminConfigurations
-  {
-    allow_instant_ach :: Bool
-  } deriving (Show, Generic, FromJSON, ToJSON)
+newtype AdminConfigurations = AdminConfigurations {allow_instant_ach :: Bool}
+  deriving (Show, Generic, FromJSON, ToJSON)
 
 data Account = Account
-  { id :: String
-  , account_number :: String
-  , status :: String
-  , user_configurations :: Maybe String
-  , crypto_status :: String
-  , admin_configurations :: AdminConfigurations
-  , currency :: String
-  , cash :: String
-  , trade_suspended_by_user :: Bool
-  , daytrade_count :: Int
-  } deriving (Show, Generic, FromJSON, ToJSON)
-
+  { id :: String,
+    account_number :: String,
+    status :: String,
+    user_configurations :: Maybe String,
+    crypto_status :: String,
+    admin_configurations :: AdminConfigurations,
+    currency :: String,
+    cash :: String,
+    trade_suspended_by_user :: Bool,
+    daytrade_count :: Int
+  }
+  deriving (Show, Generic, FromJSON, ToJSON)
