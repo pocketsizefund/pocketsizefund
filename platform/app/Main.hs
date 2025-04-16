04d1a626033a17ac9e62ea0355e174e4c4d25c0f
Chris Addy <chrisaddy@Chriss-MacBook-Air.local>
2025-04-15 21:12:02 -0400

    add GET /account endpoint

--- platform/app/Main.hs
+++ platform/app/Main.hs
@@ -1,4 +1,6 @@
 module Main (main) where
+import Network.Wai.Handler.Warp (run, runSettings, defaultSettings, setLogger, setPort)
+import Network.Wai.Logger (withStdoutLogger)
 
 import Lib
 import Network.Wai.Handler.Warp (defaultSettings, run, runSettings, setLogger, setPort)
@@ -8,3 +10,4 @@ main :: IO ()
 main = withStdoutLogger $ \aplogger -> do
   let settings = setPort 8080 $ setLogger aplogger defaultSettings
   runSettings settings app
+
