379751716b7da2be1ed666000f7bcad877003dbe
chrisaddy <chris.william.addy@gmail.com>
2025-05-15 01:17:53 -0400

    linting add yamllint and lint current yaml files
    infrastructure for gcp cloud run

---
+++ infrastructure/project.py
@@ -0,0 +1,24 @@
+import pulumi
+from pulumi_gcp.projects import Service, IAMMember
+from pulumi_gcp.serviceaccount import Account
+
+PROJECT = pulumi.Config("gcp").require("project")
+REGION = pulumi.Config("gcp").require("region")
+
+Service("enable-run", project=PROJECT, service="run.googleapis.com")
+Service("enable-eventarc", project=PROJECT, service="eventarc.googleapis.com")
+Service("enable-secretmanager", project=PROJECT, service="secretmanager.googleapis.com")
+Service("enable-pubsub", project=PROJECT, service="pubsub.googleapis.com")
+
+platform_service_account = Account(
+    "platform-service-account",
+    account_id="platform",
+    display_name="Platform-Wide Cloud Run Service Account",
+)
+
+IAMMember(
+    "pubsub-token-access",
+    project=PROJECT,
+    role="roles/pubsub.subscriber",
+    member=platform_service_account.email.apply(lambda e: f"serviceAccount:{e}"),
+)
