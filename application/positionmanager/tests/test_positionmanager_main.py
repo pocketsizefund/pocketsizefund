66249bbb6b768db1464a356bafb6e7b214457357
chrisaddy <chris.william.addy@gmail.com>
2025-05-28 16:41:39 -0400

    no blind exceptions
    update to fix blind exceptions

--- application/positionmanager/tests/test_positionmanager_main.py
+++ application/positionmanager/tests/test_positionmanager_main.py
@@ -1,4 +1,6 @@
 from fastapi.testclient import TestClient
+from fastapi import HTTPException
+
 import unittest
 from unittest.mock import patch, MagicMock
 import polars as pl
@@ -81,7 +83,9 @@ class TestPositionsEndpoint(unittest.TestCase):
     @patch("application.positionmanager.src.positionmanager.main.AlpacaClient")
     def test_create_position_alpaca_error(self, MockAlpacaClient: MagicMock) -> None:
         mock_alpaca_instance = MagicMock(spec=AlpacaClient)
-        mock_alpaca_instance.get_cash_balance.side_effect = Exception("API error")
+        mock_alpaca_instance.get_cash_balance.side_effect = HTTPException(
+            status_code=500, detail="Error getting cash balance"
+        )
         MockAlpacaClient.return_value = mock_alpaca_instance
 
         payload = {"predictions": {"AAPL": 0.8}}
@@ -121,13 +125,15 @@ class TestPositionsEndpoint(unittest.TestCase):
         MockAlpacaClient: MagicMock,
     ) -> None:
         mock_alpaca_instance = MagicMock(spec=AlpacaClient)
-        mock_alpaca_instance.clear_positions.side_effect = Exception("API error")
+        mock_alpaca_instance.clear_positions.side_effect = HTTPException(
+            status_code=500, detail="Error getting cash balance"
+        )
         MockAlpacaClient.return_value = mock_alpaca_instance
 
         response = client.delete("/positions")
 
         assert response.status_code == 500
-        assert "API error" in response.json()["detail"]
+        assert "Error" in response.json()["detail"]
 
         MockAlpacaClient.assert_called_once()
         mock_alpaca_instance.clear_positions.assert_called_once()
