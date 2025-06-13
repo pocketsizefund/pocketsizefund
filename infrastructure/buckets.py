2714f34f8d0919df7922fe697a23c441f97aadee
John Forstmeier <john.forstmeier@gmail.com>
2025-06-12 21:29:13 -0400

    Add missing bucket resource definition

--- infrastructure/buckets.py
+++ infrastructure/buckets.py
@@ -13,8 +13,24 @@ production_data_bucket = storage.Bucket(
 
 
 storage.BucketIAMMember(
-    "platform-object-admin-access",
+    "platform-object-administrator-access",
     bucket=production_data_bucket.name,
+    role="roles/storage.objectAdmin",
+    member=project.platform_service_account.email.apply(
+        lambda e: f"serviceAccount:{e}"
+    ),
+)
+
+grafana_dashboards_bucket = storage.Bucket(
+    "grafana-dashboards-bucket",
+    name=config.require_secret("grafana_dashboards_bucket_name"),
+    location=project.REGION,
+    uniform_bucket_level_access=True,
+)
+
+storage.BucketIAMMember(
+    "grafana-dashboards-bucket-object-administrator-access",
+    bucket=grafana_dashboards_bucket.name,
     role="roles/storage.objectAdmin",
     member=project.platform_service_account.email.apply(
         lambda e: f"serviceAccount:{e}"
